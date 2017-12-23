b093c79011618547fcda9725ee494fc43c0a7e4d
rgdroid <ronny.georgi@gmail.com>
2017-12-24 00:56:27 +0100

    display implementation (buttons 1,2,3) complete (without calculation)

--- app/src/main/java/com/example/ronny/calculatordemo/MainActivity.java
+++ app/src/main/java/com/example/ronny/calculatordemo/MainActivity.java
@@ -2,12 +2,53 @@ package com.example.ronny.calculatordemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mResultString = "";
+    private TextView tv_display_main;
+
+    private Button btn_one;
+    private Button btn_two;
+    private Button btn_three;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_display_main = findViewById(R.id.tv_display);
+
+        btn_one = (Button) findViewById(R.id.btn_number_one);
+        btn_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mResultString += "1";
+                tv_display_main.setText(mResultString);
+            }
+        });
+
+        btn_two = (Button) findViewById(R.id.btn_number_two);
+        btn_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mResultString += "2";
+                tv_display_main.setText(mResultString);
+            }
+        });
+
+        btn_three = (Button) findViewById(R.id.btn_number_three);
+        btn_three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mResultString += "3";
+                tv_display_main.setText(mResultString);
+            }
+        });
     }
+
+
 }
